58f3422e0276de16e89487cdf91d2417e51df581
Jeffry Langford <jblangford97@gmail.com>
2023-03-13 00:09:34 -0500

    full functionality, read me

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# taker-of-the-notes
+
+# Note Taker
+
+## Description
+
+Note Taker is an app to help organize different thoughts, comments and other things similar to the note app in many smart phones nowadays.
+
+## Link to Application
+
+
+## Screenshot
+
+![alt text](assets/screenshot.jpg)
+
+## Usage Information
+
+Create a title for your note with any amount of text. When finished, hit "Create Note" and it will be added to the list to the left. You can click on any note to see its text, and then delete one if you click the trash can next to it.
+
+---
+
+## Questions
+
+[Github](https://github.com/Jeffry_Langford)
+
+[Email](jblangford97@gmail.com)
--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { findById, createNewNote, validateNote, } = require("../../lib/notes");
 const { notes } = require("../../db/db.json");
+const fs = require('fs');
 
 router.get('/notes', (req, res) => {
     let results = notes;
@@ -26,6 +27,28 @@ router.post('/notes', (req, res) => {
     const note = createNewNote(req.body, notes);
     res.json(note);
   }
+});
+
+router.delete('/notes/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = notes.findIndex(notes => parseInt(notes.id) === id);
+
+  if (index === -1) {
+    // Note not found
+    console.log(`Note with ID ${id} not found`);
+    return res.status(404).send('Note not found');
+  }
+
+  // remove note from array
+  notes.splice(index, 1);
+  console.log(`Deleted note with ID ${id}`);
+
+  // Write the updated data array back to the file
+  let data = '{"notes":' + JSON.stringify(notes) + "}";
+  fs.writeFileSync("db/db.json", data);
+
+  // Return a success message
+  res.send(`Note ${id} deleted successfully`);
 });
 
 module.exports = router;
